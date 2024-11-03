76dc4d3ce79ddb72864e0e7609b3e9e1ba5093bf
Thiago Negri <evohunz@gmail.com>
2024-11-03 19:37:38 -0300

    Add 1..9 keys

--- wezterm.lua
+++ wezterm.lua
@@ -38,6 +38,17 @@ config.keys = {
 	{ key = "[", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
 	{ key = "]", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
 
+	-- Go to tab, 1 .. 9
+	{ key = "1", mods = "LEADER", action = wezterm.action.ActivateTab(0) },
+	{ key = "2", mods = "LEADER", action = wezterm.action.ActivateTab(1) },
+	{ key = "3", mods = "LEADER", action = wezterm.action.ActivateTab(2) },
+	{ key = "4", mods = "LEADER", action = wezterm.action.ActivateTab(3) },
+	{ key = "5", mods = "LEADER", action = wezterm.action.ActivateTab(4) },
+	{ key = "6", mods = "LEADER", action = wezterm.action.ActivateTab(5) },
+	{ key = "7", mods = "LEADER", action = wezterm.action.ActivateTab(6) },
+	{ key = "8", mods = "LEADER", action = wezterm.action.ActivateTab(7) },
+	{ key = "9", mods = "LEADER", action = wezterm.action.ActivateTab(8) },
+
 	-- New tab
 	{ key = "n", mods = "LEADER", action = wezterm.action.SpawnTab("DefaultDomain") },
 
